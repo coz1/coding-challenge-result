74a2233844a1bf1cb5201e5ad8e7a84deb42c806
Cihan Oeztuerk <cihan.ozturk@live.de>
2021-05-16 23:33:08 +0200

    - impl gradl dependenc. and plugins

--- Backend/mapping-kotlin/build.gradle.kts
+++ Backend/mapping-kotlin/build.gradle.kts
@@ -4,6 +4,8 @@ plugins {
 	val kotlinVersion = "1.4.21"
 	kotlin("jvm") version kotlinVersion
 	kotlin("plugin.spring") version kotlinVersion
+	kotlin("kapt") version "1.5.0"
+
 	id("org.springframework.boot") version "2.4.0"
 	id("io.spring.dependency-management") version "1.0.10.RELEASE"
 }
@@ -18,6 +20,17 @@ repositories {
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+
+	implementation("org.mapstruct:mapstruct:1.4.2.Final")
+	annotationProcessor("org.mapstruct:mapstruct-processor:1.4.2.Final")
+	testAnnotationProcessor("org.mapstruct:mapstruct-processor:1.4.2.Final")
+
+	implementation("junit:junit:4.13.2")
+
+	implementation("org.mockito:mockito-core:3.10.0")
+	testImplementation("org.mockito:mockito-inline:2.13.0")
+	kapt("org.mapstruct:mapstruct-processor:1.4.2.Final")
+
 }
 
 tasks {
@@ -26,7 +39,7 @@ tasks {
 			jvmTarget = "1.8"
 			apiVersion = "1.4"
 			languageVersion = "1.4"
-			freeCompilerArgs = listOf("-Xjsr305=strict")
+			freeCompilerArgs = listOf("-Xjsr305=strict","-Xjvm-default=all-compatibility")
 		}
 	}
 	withType<Test> {
