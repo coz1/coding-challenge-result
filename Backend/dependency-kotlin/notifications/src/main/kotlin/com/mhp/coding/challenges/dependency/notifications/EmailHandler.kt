05a13f387f1c8f6acc04a40a0cc7d9e801ad7bc4
Cihan Oeztuerk <cihan.ozturk@live.de>
2021-05-18 17:48:08 +0200

    - lol @service anno

--- Backend/dependency-kotlin/notifications/src/main/kotlin/com/mhp/coding/challenges/dependency/notifications/EmailHandler.kt
+++ Backend/dependency-kotlin/notifications/src/main/kotlin/com/mhp/coding/challenges/dependency/notifications/EmailHandler.kt
@@ -1,9 +1,7 @@
 package com.mhp.coding.challenges.dependency.notifications
 
 import com.mhp.coding.challenges.dependency.inquiry.Inquiry
-import com.mhp.coding.challenges.dependency.notifications.EmailHandler
 import mu.KotlinLogging
-import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Component
 
 private val logger = KotlinLogging.logger {}
