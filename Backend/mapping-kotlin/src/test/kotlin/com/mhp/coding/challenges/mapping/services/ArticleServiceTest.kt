b522049eae4e7c5ecf6bf39817f6ac256325b45f
Cihan Oeztuerk <cihan.ozturk@live.de>
2021-05-18 21:19:22 +0200

    - generate AricleServiceTest

--- Backend/mapping-kotlin/src/test/kotlin/com/mhp/coding/challenges/mapping/services/ArticleServiceTest.kt
+++ Backend/mapping-kotlin/src/test/kotlin/com/mhp/coding/challenges/mapping/services/ArticleServiceTest.kt
@@ -1,5 +1,32 @@
 package com.mhp.coding.challenges.mapping.services
 
-import org.junit.jupiter.api.Assertions.*
+import com.mhp.coding.challenges.mapping.mappers.ICustomMapper
+import com.mhp.coding.challenges.mapping.mappers.ICustomMapperImpl
+import com.mhp.coding.challenges.mapping.models.db.Article
+import com.mhp.coding.challenges.mapping.models.dto.ArticleDto
+import com.mhp.coding.challenges.mapping.repositories.ArticleRepository
+import io.mockk.impl.annotations.MockK
+import org.junit.jupiter.api.Test
 
-internal class ArticleServiceTest
+
+import org.springframework.beans.factory.annotation.Autowired
+
+internal class ArticleServiceTest{
+
+    @MockK
+    private val articleService = ArticleService()
+
+    @Autowired
+    lateinit var customMapper: ICustomMapper
+
+
+    @Test
+    fun bl() {
+        val article = ArticleRepository.findBy(1001L)
+        val articles  = article?.let { customMapper.toArticleDto(it) }
+
+
+    }
+
+
+}
