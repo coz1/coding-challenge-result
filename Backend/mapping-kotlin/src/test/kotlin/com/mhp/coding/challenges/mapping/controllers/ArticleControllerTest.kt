bec510dfd8f3907acc84472407c99a3888e3d0bf
Cihan Oeztuerk <cihan.ozturk@live.de>
2021-05-18 21:18:54 +0200

    - generate ArticleControllerIntegrationTest

--- Backend/mapping-kotlin/src/test/kotlin/com/mhp/coding/challenges/mapping/controllers/ArticleControllerTest.kt
+++ Backend/mapping-kotlin/src/test/kotlin/com/mhp/coding/challenges/mapping/controllers/ArticleControllerTest.kt
@@ -1,7 +1,10 @@
 package com.mhp.coding.challenges.mapping
 
-import com.mhp.coding.challenges.mapping.models.db.Article
+import netscape.javascript.JSObject
 import org.assertj.core.api.Assertions.assertThat
+import org.json.JSONArray
+import org.json.JSONException
+import org.json.JSONObject
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
@@ -9,11 +12,10 @@ import org.springframework.boot.test.web.client.TestRestTemplate
 import org.springframework.boot.test.web.client.getForEntity
 import org.springframework.boot.web.server.LocalServerPort
 import org.springframework.http.HttpStatus
-import javax.annotation.PostConstruct
 
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-class ArticleControllerIntegrationTest(@Autowired val restTemplate: TestRestTemplate) {
+class ArticleControllerTest(@Autowired val restTemplate: TestRestTemplate) {
     @LocalServerPort
     private val port = 0
 
@@ -48,22 +50,7 @@ class ArticleControllerIntegrationTest(@Autowired val restTemplate: TestRestTemp
 
         val entity = restTemplate.getForEntity<String>("$URI/1001")
         assertThat(entity.statusCode).isEqualTo(HttpStatus.OK)
-        println(entity.body)
         assertThat(entity.body).contains(id,author,title,description)
     }
-
-    @Test
-    fun `Testing content when Response contains wrong propertie`() {
-        val id = "1001"
-        val author = "Erika Musterfrau"
-        val description = "Article Description 1001"
-        val title = "Article Nr.: 1001"
-
-        val entity = restTemplate.getForEntity<String>("$URI/1001")
-        assertThat(entity.statusCode).isEqualTo(HttpStatus.OK)
-        //assert(entity.body).contains(id,author,title,description)
-    }
-
-
 
 }
