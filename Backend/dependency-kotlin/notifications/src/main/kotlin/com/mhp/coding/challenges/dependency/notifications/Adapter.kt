5e2563368b2a9138357fecf7a242ff00ab443253
Cihan Oeztuerk <cihan.ozturk@live.de>
2021-05-18 17:43:07 +0200

    - extend by InquiryHandler (I)

--- Backend/dependency-kotlin/notifications/src/main/kotlin/com/mhp/coding/challenges/dependency/notifications/Adapter.kt
+++ Backend/dependency-kotlin/notifications/src/main/kotlin/com/mhp/coding/challenges/dependency/notifications/Adapter.kt
@@ -1,9 +1,14 @@
 package com.mhp.coding.challenges.dependency.notifications
 
+import com.mhp.coding.challenges.dependency.inquiry.Inquiry
+import com.mhp.coding.challenges.dependency.inquiry.InquiryHandler
 import org.springframework.beans.factory.annotation.Autowired
 
 class Adapter(
     @Autowired private val emailHandler: EmailHandler,
-    @Autowired private val pushNotificationHandler: PushNotificationHandler
-) {
+    @Autowired private val pushNotificationHandler: PushNotificationHandler,
+): InquiryHandler {
+    override fun handle(inquiry: Inquiry) {
+
+    }
 }
