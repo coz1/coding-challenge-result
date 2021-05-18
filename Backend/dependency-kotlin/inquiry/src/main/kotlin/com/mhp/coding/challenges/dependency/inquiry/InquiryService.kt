4f1c7e154d130b976b1b2cb12d62e15df304a926
Cihan Oeztuerk <cihan.ozturk@live.de>
2021-05-18 17:36:23 +0200

    - impl constru. variable as autowired and iterate about list

--- Backend/dependency-kotlin/inquiry/src/main/kotlin/com/mhp/coding/challenges/dependency/inquiry/InquiryService.kt
+++ Backend/dependency-kotlin/inquiry/src/main/kotlin/com/mhp/coding/challenges/dependency/inquiry/InquiryService.kt
@@ -1,16 +1,20 @@
 package com.mhp.coding.challenges.dependency.inquiry
 
 import mu.KotlinLogging
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
 
 private val logger = KotlinLogging.logger {}
 
 @Component
-class InquiryService {
+class InquiryService(
+    @Autowired var handlers: List<InquiryHandler>,
+){
     fun create(inquiry: Inquiry) {
         logger.info {
             "User sent inquiry: $inquiry"
         }
+        handlers.forEach { it.handle(inquiry) }
     }
 }
 
